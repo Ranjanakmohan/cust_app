efe5d9bc288cf6b4b93f64625281f4c9d0db02e4
Ranjanakmohan <ranjanakmohan@gmail.com>
2022-02-12 15:54:18 +0530

    report

---
+++ cust_app/cust_app/report/sales_report/sales_report.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2022, momscode and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Sales report"] = {
+	"filters": [
+
+	]
+};
